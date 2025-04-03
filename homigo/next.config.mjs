2707c403eaff77bc24bb704ff31b5a33dd4eaf9c
lonebono <164330238+lonebono@users.noreply.github.com>
2025-04-03 08:45:07 +0800

    Update next.config.mjs

--- homigo/next.config.mjs
+++ homigo/next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  reactStrictMode: true,
+  experimental: {
+    appDir: true,
+  }, {
   images: {
     domains: ["res.cloudinary.com"], // Cloudinary domain added for image optimization
   },
